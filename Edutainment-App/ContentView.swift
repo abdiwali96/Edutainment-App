b02f7f5999beb083d2434fcc7b19db33189fc7b8
abdiwali96 <abdiwali96@gmail.com>
2020-09-23 13:44:17 +0100

    Initial Commit

---
+++ Edutainment-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Edutainment-App
+//
+//  Created by Abdiwali Abdi on 23/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Edutainment-App/Edutainment_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Edutainment_AppApp.swift
+//  Edutainment-App
+//
+//  Created by Abdiwali Abdi on 23/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct Edutainment_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
